a877c6116ab9f21695e40b83cbbcc7ac9e97dbe2
Umar-Farooq-Afridi <umerfarooq21441@gmail.com>
2026-08-13 12:09:28 +0500

    Setup frontend for Task Manager App (ReactJS + TypeScript)

---
+++ Task-19/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Task Manager App - Typescript</h1>
+      <p>React + TypeScript</p>
+    </div>
+  );
+}
+
+export default App;
